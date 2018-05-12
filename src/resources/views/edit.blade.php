dd85564169a199996408afb1118a223d3fd7c783
feripratama <eroorsys@gmail.com>
2018-05-12 10:46:06 +0700

    Update controller, add new relation on model
     On branch master
     Changes to be committed:
    	new file:   src/.editorconfig
    	modified:   src/resources/views/edit.blade.php

--- src/resources/views/edit.blade.php
+++ src/resources/views/edit.blade.php
@@ -1,149 +1,53 @@
-<?php namespace Bantenprov\DaftarRetribusi\Http\Controllers;
+@extends('master') 
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Bantenprov\DaftarRetribusi\Facades\DaftarRetribusi;
-use Bantenprov\DaftarRetribusi\Models\DaftarRetribusiModel;
-use Ramsey\Uuid\Uuid;
-use Bantenprov\LaravelOpd\Models\LaravelOpdModel;
+@section('content')
 
-/**
- * The DaftarRetribusiController class.
- *
- * @package Bantenprov\DaftarRetribusi
- * @author  bantenporv <developer.bantenprov@gmail.com>
- */
-class DaftarRetribusiController extends Controller
-{    
+<div class="container-fluid">
+	<div class="animated fadeIn">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1>Edit Daftar Retribusi</h1>
+				<hr>
+				<div class="card">
+					<div class="card-header">
+						Input <strong>Sizes</strong>
+					</div>
+					<div class="card-body">
+						<form action="{{route('daftar-retribusi.update', $daftar_retribusi->id)}}" method="POST">
+							<input type="hidden" name="_method" value="PUT"> {{ csrf_field() }}
+							<div class="form-group">
+								<label for="title">Title</label>
+								<input type="text" value="{{$daftar_retribusi->title}}" class="form-control" id="taskTitle" name="title">
+							</div>
+							<div class="form-group">
+								<label for="description">Description</label>
+								<input type="text" value="{{$daftar_retribusi->description}}" class="form-control" id="taskDescription" name="description">
+							</div>
+              <div class="form-group">
+                <label for="opd_id">Level Unit Kerja {{ $daftar_retribusi->getOpd->name }}</label>
+                <select id="opd_id" name="opd_id" class="form-control form-control">
+                  <option value="">Please select</option>
+                  @foreach ($opds as $opd)
+                    <option value="{{$opd->id}}" {{ ($opd->id ==  $daftar_retribusi->opd_id) ? 'selected' : '' }}>{{$opd->name}}</option>    
+                  @endforeach                                   
+                </select>
+              </div>
+							@if ($errors->any())
+							<div class="alert alert-danger">
+								<ul>
+									@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+									@endforeach
+								</ul>
+							</div>
+							@endif
+							<button type="submit" class="btn btn-primary">Submit</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $daftar_retribusies = DaftarRetribusiModel::with('getOpd')->get();
-        
-        
-        return view('daftar-retribusi::index', compact('daftar_retribusies'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $opds = LaravelOpdModel::where('levelunker', '=', 1)->get();
-        
-        return view('daftar-retribusi::create', compact('opds'));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'title'         => 'required|min:3',
-            'description'   => 'required',
-            'opd_id'        => 'required'
-        ]);
-
-        $opd = LaravelOpdModel::find($request->opd_id);
-
-        if(is_null($opd)){
-            return redirect()->back()->withErrors('Can\'t find opd.');
-        }
-        
-
-        $daftar_retribusi = DaftarRetribusiModel::create(
-                            [
-                                'id'            => Uuid::uuid5(Uuid::NAMESPACE_DNS, 'bantenprov.go.id'.date('YmdHis')),
-                                'title'         => $request->title,
-                                'description'   => $request->description,
-                                'opd_id'        => $request->opd_id
-                            ]);
-        
-                            
-        return redirect()->route('daftar-retribusi.index')->with('message', 'Success add new data.');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $daftar_retribusi = DaftarRetribusiModel::find($id);
-
-        return view('daftar-retribusi::show', compact('daftar_retribusi'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $daftar_retribusi = DaftarRetribusiModel::with('getOpd')->find($id);
-
-        $opds = LaravelOpdModel::where('levelunker', '=', 1)->get();
-
-        return view('daftar-retribusi::edit', compact('daftar_retribusi','opds'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $request->validate([
-            'title'         => 'required|min:3',
-            'description'   => 'required',
-            'opd_id'        => 'required'
-        ]);
-
-        $opd = LaravelOpdModel::find($request->opd_id);
-
-        if(is_null($opd)){
-            return redirect()->back()->withErrors('Can\'t find opd.');
-        }
-        
-        $daftar_retribusi = DaftarRetribusiModel::find($id);
-        $daftar_retribusi->title        = $request->title;
-        $daftar_retribusi->description  = $request->description;
-        $daftar_retribusi->opd_id       = $request->opd_id;
-        $daftar_retribusi->save();
-        
-        return redirect()->route('daftar-retribusi.index')->with('message', 'Success update data.');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $daftar_retribusi = DaftarRetribusiModel::find($id);
-        $daftar_retribusi->delete();
-
-        return redirect()->route('daftar-retribusi.index')->with('message', 'Success deleted data.');
-
-    }
-}
+@endsection
