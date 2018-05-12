d2a3b7acd2d1e784f5501a8dc19a4ed7605ed93d
feripratama <eroorsys@gmail.com>
2018-05-12 10:39:32 +0700

    Update blade files, model add new relation, controller
     On branch master
     Changes to be committed:
    	modified:   src/Http/Controllers/DaftarRetribusiController.php
    	modified:   src/Models/DaftarRetribusiModel.php
    	modified:   src/resources/views/create.blade.php
    	modified:   src/resources/views/edit.blade.php
    	modified:   src/resources/views/index.blade.php

--- src/resources/views/create.blade.php
+++ src/resources/views/create.blade.php
@@ -1,27 +1,53 @@
-@extends('layout.master')
- 
+
+@extends('master')
 @section('content')
-    <h1>Add New Daftar Retribusi</h1>
-    <hr>
-     <form action="{{route('daftar-retribusi.store')}}" method="post">
-     {{ csrf_field() }}
-      <div class="form-group">
-        <label for="title">Daftar retribusi Title</label>
-        <input type="text" class="form-control" id="daftarRetribusiTitle"  name="title">
-      </div>
-      <div class="form-group">
-        <label for="description">Daftar retribusi Description</label>
-        <input type="text" class="form-control" id="daftarRetribusiDescription" name="description">
-      </div>
-      @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
+
+<div class="container-fluid">
+  <div class="animated fadeIn">
+    <div class="row">
+      <div class="col-lg-12">
+      <h1>Add New Daftar Retribusi</h1>
+        <hr>
+        <div class="card">
+          <div class="card-header">
+            Input <strong>Sizes</strong>
+          </div>
+          <div class="card-body">
+           <form action="{{route('daftar-retribusi.store')}}" method="post">
+             {{ csrf_field() }}
+             <div class="form-group">
+              <label for="title">Title</label>
+              <input type="text" class="form-control" id="daftarRetribusiTitle"  name="title">
+            </div>
+            <div class="form-group">
+              <label for="description">Description</label>
+              <input type="text" class="form-control" id="daftarRetribusiDescription" name="description">
+            </div>
+            <div class="form-group">
+              <label for="opd_id">Level Unit Kerja</label>
+              <select id="opd_id" name="opd_id" class="form-control form-control">
+                <option value="">Please select</option>
+                @foreach ($opds as $opd)
+                  <option value="{{$opd->id}}">{{$opd->name}}</option>    
+                @endforeach                                   
+              </select>
+            </div>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+              <ul>
                 @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
+                <li>{{ $error }}</li>
                 @endforeach
-            </ul>
+              </ul>
+            </div>
+            @endif
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
         </div>
-      @endif
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
 @endsection
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,42 +1,77 @@
-@extends('layout.master')
- 
+@extends('master')
 @section('content')
-   @if (Session::has('message'))
-    <div class="alert alert-info">{{ Session::get('message') }}</div>
-  @endif
-  <table class="table" border="1">
-    <thead class="thead-dark">
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Title</th>
-        <th scope="col">Description</th>
-        <th scope="col">Created At</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($daftar_retribusies as $daftar_retribusi)
-      <tr>
-        <th scope="row">{{$daftar_retribusi->id}}</th>
-        <td>
-          <a href="{{route('daftar-retribusi.show',$daftar_retribusi->id)}}">{{$daftar_retribusi->title}}</a>
-        </td>
-        <td>{{$daftar_retribusi->description}}</td>
-        <td>{{$daftar_retribusi->created_at->toFormattedDateString()}}</td>
-        <td>
-          <div class="btn-group" role="group" aria-label="Basic example">
-            <a href="{{ route('daftar-retribusi.edit',$daftar_retribusi->id) }}">
-              <button type="button" class="btn btn-warning">Edit</button>
+@if (Session::has('message'))
+<div class="alert alert-info">{{ Session::get('message') }}</div>
+@endif
+
+<div class="container-fluid">
+  <div class="animated fadeIn">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card">
+          <div class="card-header">
+            <i class="fa fa-align-justify"></i> Daftar Retribusi
+            <a href="{{ route('daftar-retribusi.create') }}" class="float-right">
+              <button type="button" class="btn btn-warning">Add new</button>
             </a>&nbsp;
-            <form action="{{route('daftar-retribusi.destroy',$daftar_retribusi->id)}}" method="POST">
-              <input type="hidden" name="_method" value="DELETE">
-              <input type="hidden" name="_token" value="{{ csrf_token() }}">
-              <input type="submit" class="btn btn-danger" value="Delete" />
-            </form>
           </div>
-        </td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
+          <div class="card-body">
+            <table class="table table-responsive-sm table-bordered table-striped table-sm">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">OPD</th>
+                  <th scope="col">Created At</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach($daftar_retribusies as $key=>$daftar_retribusi)
+                <tr>
+                  <th scope="row" uuid="{{$daftar_retribusi->id}}">{{ ++$key }}</th>
+                  <td>
+                    <a href="{{route('daftar-retribusi.show',$daftar_retribusi->id)}}">{{$daftar_retribusi->title}}</a>
+                  </td>
+                  <td>{{$daftar_retribusi->description}}</td>
+                  <td>{{$daftar_retribusi->getOpd->name}}</td>
+                  <td>{{$daftar_retribusi->created_at->toFormattedDateString()}}</td>
+                  <td>
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                      <a href="{{ route('daftar-retribusi.edit',$daftar_retribusi->id) }}">
+                        <button type="button" class="btn btn-warning">Edit</button>
+                      </a>&nbsp;
+                      <form action="{{route('daftar-retribusi.destroy',$daftar_retribusi->id)}}" method="POST">
+                        <input type="hidden" name="_method" value="DELETE">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <input type="submit" class="btn btn-danger" value="Delete" />
+                      </form>
+                    </div>
+                  </td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+            <nav>
+              <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#">Prev</a></li>
+                <li class="page-item active">
+                  <a class="page-link" href="#">1</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <!--/.col-->
+    </div>
+    <!--/.row-->
+  </div>
+</div>
+
 @endsection
